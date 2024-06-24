8c00294a951ae5e9544a7ca6d87718a6322c9e6a
Mmeso1 <anisiubammesoma8@gmail.com>
2024-06-24 23:22:47 +0100

    removed document.ready

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,8 +1,6 @@
 // src/index.js
-import $ from "jquery";
+const $ = require("jquery");
 
-$(document).ready(function () {
-  $("body").append("<p>Paragraph 1</p>");
-  $("body").append("<p>Paragraph 2</p>");
-  $("body").append("<p>Paragraph 3</p>");
-});
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<p>Copyright - Holberton School</p>");
