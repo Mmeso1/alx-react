bff76d2f78655713beeee213f7209c70fc4aecc7
Mmeso1 <anisiubammesoma8@gmail.com>
2024-06-25 08:27:49 +0100

    task 2 pls work

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -13,12 +13,21 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: "asset/resource",
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
       },
     ],
   },
