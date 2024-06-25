30af0d366795c30cb6d2d897aa667008af4143e1
Mmeso1 <anisiubammesoma8@gmail.com>
2024-06-25 01:15:31 +0100

    task 2

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,15 @@
+const $ = require("jquery");
+const _ = require("lodash");
+
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<button>Click here to get started</button>");
+$("body").append("<p id='count'></p>");
+$("body").append("<p>Copyright - Holberton School</p>");
+
+function updateCounter() {
+  const count = $("#count").text() || 0;
+  $("#count").text(`${parseInt(count) + 1} clicks on the button`);
+}
+
+$("button").on("click", _.debounce(updateCounter, 300));
