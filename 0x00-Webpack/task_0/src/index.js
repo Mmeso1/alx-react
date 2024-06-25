e3385937c4a18883ad35c98b3040705d7c696875
Mmeso1 <anisiubammesoma8@gmail.com>
2024-06-25 06:47:58 +0100

    task0 index fix

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,6 +1,5 @@
-// src/index.js
 const $ = require("jquery");
-
-$("body").append("<p>Holberton Dashboard</p>");
-$("body").append("<p>Dashboard data for Holberton School</p>");
-$("body").append("<p>Copyright - Holberton School</p>");
+ 
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
