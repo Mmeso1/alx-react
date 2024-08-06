0811e0e77df6362afc0f1fc3ad41694e5cac1686
Mmeso1 <anisiubammesoma8@gmail.com>
2024-08-06 07:16:19 +0100

    need to understand the reducer concept

--- 0x06-React_Immutable/what.js
+++
@@ -1,7 +0,0 @@
-// Import any necessary modules or libraries here
-
-// Define your functions or variables here
-
-// Write your code here
-
-// Export any functions or variables that need to be accessed from other files
